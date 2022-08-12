b20be403df590e4321b32ab876608580866b594f
H Enes Simsek <enes1406@gmail.com>
2022-08-12 10:05:52 +0300

    adding a feature to alert when user wants to use app in irrelevant page

--- popup.js
+++ popup.js
@@ -4,19 +4,25 @@ console.log("popup.js has been started.");
 
 startBanFavorites.onclick = function(element) {
 	
-	
 	chrome.tabs.query({}, async function(tabs) {
 		for (let tab of tabs) {
 			// get active tab
 			if(tab.active)
 			{
-				//alert(tab.url);
-				
-				// execute content script1
-				console.log("popup.js: scriptScrapAuthorsFromEntry will be exec");
-				chrome.scripting.executeScript({ target: {tabId: tab.id}, files: ['scriptScrapAuthorsFromEntry.js'] }, function() {
-					console.log("scriptScrapAuthorsFromEntry has been executed.");
-				});
+				if(tab.url.includes("https://eksisozluk.com/entry"))
+				{
+					// execute content script1
+					console.log("popup.js: scriptScrapAuthorsFromEntry will be exec");
+					chrome.scripting.executeScript({ target: {tabId: tab.id}, files: ['scriptScrapAuthorsFromEntry.js'] }, function() {
+						console.log("scriptScrapAuthorsFromEntry has been executed.");
+					});
+				}
+				else
+				{
+					// the extension should be used in an ekşisözlük entry page
+					console.log("popup.js: scriptScrapAuthorsFromEntry will not be executed, current url: " + tab.url);
+					alert("Bu eklenti sadece bir ekşisözlük entry sayfası açıkken kullanılabilir. Örneğin https://eksisozluk.com/entry/1");
+				}
 			}
 		}
 	}); 
--- scriptScrapAuthorsFromEntry.js
+++ scriptScrapAuthorsFromEntry.js
@@ -1,18 +1,22 @@
 if (document.readyState == "complete") {
-	// click to open the list of authors
-  let dropDownListElement = document.getElementsByClassName("favorite-count")[1];
-	dropDownListElement.click();
 	
 	let dropDownList;
 	let values = [];
 	
 	try
 	{
+		// click to open the list of authors
+		let dropDownListElement = document.getElementsByClassName("favorite-count")[1];
+		dropDownListElement.click();
+		
 		// get list of authors
 		dropDownList = document.getElementsByClassName("favorite-list-popup toggles-menu open")[0].firstChild.firstChild.getElementsByTagName("li");
 		for(var i=0;i < dropDownList.length; i++) {
 			if (dropDownList[i].innerText.length) { values.push((dropDownList[i].innerText.replace(/\s/g,''))); }
 		}
+		
+		// click to close the list of authors
+		dropDownListElement.click();
 		
 		// delete '@' char from nicknames
 		for(var i=0;i < values.length; i++) {
@@ -23,28 +27,28 @@ if (document.readyState == "complete") {
 		if(values[values.length-1].substr(-6,6) === "çaylak")
 			values.pop()
 		
-		let authorListString = values.join("\n");
+		console.log("number of authors obtained: " + values.length);
 		
-		// save the list to local storage api
-		chrome.storage.local.set({"userList": authorListString }, function(){
-			if(!chrome.runtime.error){
-				console.log("Author list saved into local storage");
-				// send start msg to background.js
-				chrome.runtime.sendMessage(null, "scriptScrapAuthorsFromEntry::start");
-			}else{
-				console.log("chrome.storage.local.set runtime error");
-				alert("chrome.storage.local.set runtime error");
-			}
-		});
+		if(values.length > 0)
+		{
+			let authorListString = values.join("\n");
 		
-		console.log("number of authors obtained: " + len(authorListString));
+			// save the list to local storage api
+			chrome.storage.local.set({"userList": authorListString }, function(){
+				if(!chrome.runtime.error){
+					console.log("Author list saved into local storage");
+					// send start msg to background.js
+					chrome.runtime.sendMessage(null, "scriptScrapAuthorsFromEntry::start");
+				}else{
+					console.log("chrome.storage.local.set runtime error");
+					alert("chrome.storage.local.set runtime error");
+				}
+			});
+		}
+		
 	}
 	catch
 	{
 		console.log("scriptScrapAuthorsFromEntry: Author list could not be obtained.");
 	}
-	
-	// click to close the list of authors
-	dropDownListElement.click();
-
 }
