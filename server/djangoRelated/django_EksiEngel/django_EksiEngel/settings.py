f30d09c1aad3c25bd07771a45a9eb62ef180b666
h-enes-simsek <1425963@gmail.com>
2022-11-17 12:57:16 +0300

    fix bug: debug option was not working correctly with .env file in django settings.py

--- server/djangoRelated/django_EksiEngel/django_EksiEngel/settings.py
+++ server/djangoRelated/django_EksiEngel/django_EksiEngel/settings.py
@@ -14,10 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = (env("DEBUG") == "True")
 
-ALLOWED_HOSTS = ["127.0.0.1"]
-
+ALLOWED_HOSTS = [".eksiengel.hesimsek.com", "165.227.163.107", "localhost"]
 
 # Application definition
 
