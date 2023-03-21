08a7c1745438b0a83cd95684692afd29d7e0f6eb
h-enes-simsek <1425963@gmail.com>
2023-03-21 17:33:01 +0300

    fav ban button removed from popup. edited user manual in faq.

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,43 +2,16 @@
 
 console.log("popup.js: has been started.");
 
-startBanFavorites.onclick = function(element) {
-	
-	chrome.tabs.query({}, async function(tabs) {
-		for (let tab of tabs) {
-			// get active tab
-			if(tab.active)
-			{
-				if(tab.url.includes("eksisozluk.com/entry"))
-				{
-          // send message to background page
-          chrome.runtime.sendMessage(null, {"banSource":"FAV", "banMode":"BAN", "entryUrl": tab.url}, function (){
-            // automatically close the popup.html if operation is successful
-            window.close();
-          });
-          
-				}
-				else
-				{
-					// the extension should be used in an ekşisözlük entry page
-					console.log("popup.js: wrong url, current url: " + tab.url);
-					alert("Favorileyenleri engellemek istediğiniz ekşisözlük entry'sini açın.\nÖrneğin: https://eksisozluk.com/entry/1");
-				}
-			}
-		}
-	}); 
-};
-
-startUndobanAll.onclick = function(element) {
-	// send message to background page
-	chrome.runtime.sendMessage(null, {"banSource":"UNDOBANALL", "banMode":"UNDOBAN"});
-};
-
 openauthorListPage.onclick = function(element) {
   chrome.tabs.create({ url: chrome.runtime.getURL("assets/html/authorListPage.html") }, function (){
   // automatically close the popup.html if operation is successful
     window.close();
   });
+};
+
+startUndobanAll.onclick = function(element) {
+	// send message to background page
+	chrome.runtime.sendMessage(null, {"banSource":"UNDOBANALL", "banMode":"UNDOBAN"});
 };
 
 openFaq.onclick = function(element) {
