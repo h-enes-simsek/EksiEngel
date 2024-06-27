55db71f194b4ec0a66b416079c06d970f1c22fbe
h-enes-simsek <1425963@gmail.com>
2024-06-27 21:27:33 +0200

    updated admin panel such that in an Action record raw_id_fields will prevent loading all the values of fields defined with ForeignKey by not using drop-down menu. this reduces total fetched bytes in admin panel

--- backend/django_EksiEngel/api/admin.py
+++ backend/django_EksiEngel/api/admin.py
@@ -27,10 +27,14 @@ admin.site.register(ClickSource, ClickSourceAdmin)
 admin.site.register(LogLevel, LogLevelAdmin)
 admin.site.register(TimeSpecifier, TimeSpecifierAdmin)
 
+# raw_id_fields will prevent loading all the values of fields defined with ForeignKey by not using drop-down menu
+# author_list is not defined in raw_id_fields because only ids will be shown not string names, so solution:
 # author_list field in Action will be filtered so, 
 # only relevant EksiSozlukUser(s) will be shown in an Action record
 # instead of all EksiSozlukUser(s)
 class ActionAdmin(admin.ModelAdmin):
+    
+    # filter manytomany fields so only relevant records will be shown
     def get_object(self, request, object_id, s):
         # Hook obj for use in formfield_for_manytomany
         self.obj = super(ActionAdmin, self).get_object(request, object_id)
@@ -39,5 +43,7 @@ class ActionAdmin(admin.ModelAdmin):
         if db_field.name == "author_list":
             kwargs["queryset"] = EksiSozlukUser.objects.filter(author_list_in_action=self.obj)
         return super().formfield_for_manytomany(db_field, request, **kwargs)
-
+    
+    # do not fetch all values for drop-down menu
+    raw_id_fields = ("eksi_engel_user", "fav_title", "fav_entry", "fav_author", "fav_author")
 admin.site.register(Action, ActionAdmin)
