8a418e7157ff24f9ef8d8731b6b6c7601cdbce0a
crimeofheart <ali_duzgun@hotmail.com>
2026-03-01 23:44:12 +0100

    Fix typo

--- backend/django_EksiEngel/django_EksiEngel/settings.py
+++ backend/django_EksiEngel/django_EksiEngel/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (env("DEBUG") == "True")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".eksiengelplus.duzgun.org", "ekisengelplus.duzgun.org", "localhost", "127.0.0.1"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".eksiengelplus.duzgun.org", "eksiengelplus.duzgun.org", "localhost", "127.0.0.1"])
 
 # Application definition
 
