75d87269ab8dae579bff65da84088c5cecc0ee2f
crimeofheart <ali_duzgun@hotmail.com>
2026-03-02 00:06:59 +0100

    Add X-API-Key to CORS allowed headers

--- backend/django_EksiEngel/django_EksiEngel/settings.py
+++ backend/django_EksiEngel/django_EksiEngel/settings.py
@@ -50,6 +50,17 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-api-key',
+    'x-requested-with',
+]
 
 ROOT_URLCONF = 'django_EksiEngel.urls'
 
